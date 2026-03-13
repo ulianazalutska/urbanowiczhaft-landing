d896f2c241a83553eb1f76cf7dfd8552f5ddb1e5
Uliana Zalutska <ccode.crush@gmail.com>
2026-03-13 12:34:14 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const burger = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 burger.addEventListener("click", () => {
-mobileMenu.classList.toggle("active");
-burger.classList.toggle("active");
-document.body.classList.toggle("menu-open");
-document.documentElement.classList.toggle("menu-open");
+  mobileMenu.classList.toggle("active");
+  burger.classList.toggle("active");
+  document.body.classList.toggle("menu-open");
+  document.documentElement.classList.toggle("menu-open");
 });
